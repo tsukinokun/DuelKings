df1c12478ee78574f1cfbb823040f06228aac1ee
502-29\cre <maihei541@gmail.com>
2025-07-11 08:52:09 +0900

    [Add]InGameScene

---
+++ src/Game/AutoChess/InGameScene.h
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	InGameScene.h
+//! @brief	オートチェスのインゲームシーン
+//! @author 山﨑愛
+//---------------------------------------------------------------------------
+#pragma once
+#include <System/Scene.h>
+class InGameScene : public Scene::Base
+{
+public:
+    BP_CLASS_DECL(InGameScene, u8"オートチェスのインゲームシーン")
+    //@{
+    bool Init() override;      //!< 初期化
+    void Update() override;    //!< 更新
+    void Draw() override;      //!< 描画
+    void Exit() override;      //!< 終了
+    void GUI() override;       //!< GUI表示
+
+    //@}
+};
