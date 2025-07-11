66f4e34be662e852c9b3d7ef5cfc9b98e4d41b10
502-29\cre <maihei541@gmail.com>
2025-07-11 09:02:39 +0900

    [Add]Camera

---
+++ src/Game/AutoChess/Camera.h
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	Camera.h
+//! @brief	インゲームシーンのカメラ
+//! @author 山﨑愛
+//---------------------------------------------------------------------------
+#pragma once
+#include <System/Scene.h>
+USING_PTR(Camera);
+class Camera : public Object
+{
+public:
+    BP_OBJECT_DECL(Camera, u8"インゲームシーンのカメラ")
+    //@{
+    bool Init() override;      //!< 初期化
+    void Update() override;    //!< 更新
+    void Draw() override;      //!< 描画
+    void Exit() override;      //!< 終了
+    void GUI() override;       //!< GUI表示
+
+    //@}
+};
