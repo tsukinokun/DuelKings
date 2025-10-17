96579c64afe5567572c91a606e05f5a9c74cda1f
502-29\cre <maihei541@gmail.com>
2025-10-17 11:25:27 +0900

    [Add]ShopStand

---
+++ src/Game/AutoChess/ShopStand.h
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	ShopStand.h
+//! @brief	インゲームシーンの購入可能ピース置き場
+//! @author 山﨑愛
+//---------------------------------------------------------------------------
+#pragma once
+#include <System/Scene.h>
+USING_PTR(PieceStand);
+class ShopStand : public Object
+{
+public:
+    BP_OBJECT_DECL(PieceStand, u8"インゲームシーンの購入可能ピース置き場")
+    bool Init() override;    //!< 初期化
+
+    //オーナーの名前を設定
+    //! @param owner [in] オーナー
+    void SetOwner(std::weak_ptr<Object> owner);
+
+private:
+    std::weak_ptr<Object> owner_;    //オーナーのウィークポインタ
+};
