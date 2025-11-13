2671545acf9649e395e7b997c149f483b8bb8acf
GALLERIA\maihe <maihei541@gmail.com>
2025-11-14 00:59:37 +0900

    [Add]PieceData

--- src/Game/AutoChess/Piece/PieceData/PieceData.h
+++ src/Game/AutoChess/Piece/PieceData/PieceData.h
@@ -11,12 +11,15 @@
 //------------------------------------------------------
 struct PieceData
 {
-    std::string              type_name_;       //!< 駒の名前
-    std::string              display_name_;    //!< UI表示名
-    std::array<LevelData, 3> levels_;          //!< レベル1〜3のステータス
-    std::vector<SynergyID>   synergies_;       //!< 所属シナジー
-    std::string              icon_path_;       //!< アイコンパス
-    std::string              description_;     //!< 説明文
+    std::string              type_name_;          //!< 駒の名前
+    std::string              display_name_;       //!< UI表示名
+    std::array<LevelData, 3> levels_;             //!< レベル123のステータス
+    std::vector<SynergyID>   synergies_;          //!< 所属シナジー
+    std::string              icon_path_;          //!< アイコンパス
+    std::string              description_;        //!< 説明文
+    float                    attack_interval_;    //!< 攻撃間隔
+    float                    attack_range_;       //!< 射程距離
+    int                      price_;              //!< 購入価格
 
     //--------------------------------------------------
     //! @brief シリアライズ関数
@@ -26,6 +29,14 @@ struct PieceData
     template <class Archive>
     void serialize(Archive& ar)
     {
-        ar(CEREAL_NVP(type_name_), CEREAL_NVP(display_name_), CEREAL_NVP(levels_), CEREAL_NVP(synergies_), CEREAL_NVP(icon_path_), CEREAL_NVP(description_));
+        ar(CEREAL_NVP(type_name_),
+           CEREAL_NVP(display_name_),
+           CEREAL_NVP(levels_),
+           CEREAL_NVP(synergies_),
+           CEREAL_NVP(icon_path_),
+           CEREAL_NVP(description_),
+           CEREAL_NVP(attack_interval_),
+           CEREAL_NVP(attack_range_),
+           CEREAL_NVP(price_));
     }
 };
