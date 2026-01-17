5ebb3ba094e8974d115367df897d8b52dee1ed3a
502-29\cre <maihei541@gmail.com>
2026-01-17 12:06:59 +0900

    [Fix]JapaneseChessModel

--- src/Game/AutoChess/InGameScene.cpp
+++ src/Game/AutoChess/InGameScene.cpp
@@ -91,10 +91,6 @@ bool InGameScene::Init()
     // 机の生成
     //---------------------------------------------------------------------------------
     {
-        //auto table = Scene::Object::Create<Object>();
-        //auto model = table->AddComponent<ComponentModel>("data/AutoChess/Model/Stage/table.mv1");
-        //table->SetScaleAxisXYZ(4.0f);
-        //table->SetTranslate(float3(-0.5f, -20.0f, 0.0f));
         auto table = Scene::Object::Create<Glass>();
     }
     //---------------------------------------------------------------------------------
